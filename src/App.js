dcf9530e8af5e4d4b1e6d86e5e43e574f62e0a4f
HasanovaFidan <tu64upm8q@code.edu.az>
2023-12-26 03:58:32 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import dataContexts from './Contexts/Contexts';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ROOT from './Router/Index.routes';
+import axios from 'axios';
+function App() {
+  useEffect(() => {
+    axios.get("http://localhost:8000/products")
+      .then(res => {
+        setLoading(false);
+        setData(res.data);
+        setOriginal(res.data);
+
+      })
+      .catch(err => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+  const root=createBrowserRouter(ROOT)
+  const [data,setData]=useState([])
+  const [loading,setLoading]=useState(true)
+  const [error,setError]=useState("")
+  const [original,setOriginal]=useState([])
+  useEffect(()=>{
+    axios.get("")
+  })
+const values={
+data,
+setData,
+loading,
+setLoading,
+original,
+error,
+setError
+}
+  return (
+    <div className="App">
+    <dataContexts.Provider value={values}>
+<RouterProvider router={root}/>
+    </dataContexts.Provider>
+    </div>
+  );
+}
+
+export default App;
